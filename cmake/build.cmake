159532ca73c562f6fdda7e24cc42af86a5710a64
Vladislav Egorov <unclehook@ispras.ru>
2024-01-29 12:33:07 +0300

    use external asio and async_simple libraries. Root cmake target cinatra - INTERFACE.

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -19,6 +19,11 @@ endif()
 
 if (MSVC)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++latest")
+    # --------------------- Msvc
+    # Resolves C1128 complained by MSVC: number of sections exceeded object file format limit: compile with /bigobj.
+    add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/bigobj>")
+    # Resolves C4737 complained by MSVC: C4737: Unable to perform required tail call. Performance may be degraded. "Release-Type only"
+    add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/EHa>")
 else ()
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -pthread -std=c++20")
 endif ()
@@ -29,9 +34,3 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     #-ftree-slp-vectorize with coroutine cause link error. disable it util gcc fix.
     set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fno-tree-slp-vectorize")
 endif()
-
-# --------------------- Msvc
-# Resolves C1128 complained by MSVC: number of sections exceeded object file format limit: compile with /bigobj.
-add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/bigobj>")
-# Resolves C4737 complained by MSVC: C4737: Unable to perform required tail call. Performance may be degraded. "Release-Type only"
-add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/EHa>")
